f9fe1eedf64c3d11cb0340a697376f478a2888df
chimpdev <skyhancloud@gmail.com>
2023-10-31 04:07:33 +0300

    refactor(client): refactor client.js and index.js for better code organization and separation of concerns

---
+++ client.js
@@ -0,0 +1,33 @@
+import Discord from 'discord.js';
+import logger from './utils/logger.js';
+import dropCommand from './commands/drop.js';
+import squeakCommand from './commands/squeak.js';
+
+export default function () {
+  const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds] });
+
+  client.on('ready', () => {
+    logger.info(`Logged in as ${client.user.tag}`);
+
+    if (process.argv.includes('--register-commands')) {
+      const rest = new Discord.REST().setToken(process.env.BOT_TOKEN);
+
+      logger.info('Registering slash commands');
+      rest.put(Discord.Routes.applicationCommands(client.user.id), { body: [squeakCommand.data, dropCommand.data] }).then(() => {
+        logger.info('Successfully registered slash commands');
+      }).catch(err => logger.error(err));
+    }
+  });
+
+  client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = interaction.commandName;
+    logger.info(`Received command ${command} from ${interaction.user.tag}`);
+
+    if (command === 'squeak') return squeakCommand.execute(interaction);
+    if (command === 'drop') return dropCommand.execute(interaction);
+  });
+
+  client.login(process.env.BOT_TOKEN).catch(err => logger.error(err));
+}
--- index.js
+++ index.js
@@ -2,40 +2,14 @@ import { CronJob } from 'cron';
 import Discord from 'discord.js';
 import dotenv from 'dotenv';
 import { Database } from "quickmongo";
-import dropCommand from './commands/drop.js';
-import squeakCommand from './commands/squeak.js';
 import logger from './utils/logger.js';
 import random_squirrel_buffer from './utils/random_squirrel_buffer.js';
+import client from './client.js';
 
 dotenv.config();
 global.logger = logger;
 
-const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds] });
-
-client.on('ready', () => {
-  logger.info(`Logged in as ${client.user.tag}`);
-
-  if (process.argv.includes('--register-commands')) {
-    const rest = new Discord.REST().setToken(process.env.BOT_TOKEN);
-
-    logger.info('Registering slash commands');
-    rest.put(Discord.Routes.applicationCommands(client.user.id), { body: [squeakCommand.data, dropCommand.data] }).then(() => {
-      logger.info('Successfully registered slash commands');
-    }).catch(err => logger.error(err));
-  }
-});
-
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
-
-  const command = interaction.commandName;
-  logger.info(`Received command ${command} from ${interaction.user.tag}`);
-
-  if (command === 'squeak') return squeakCommand.execute(interaction);
-  if (command === 'drop') return dropCommand.execute(interaction);
-});
-
-client.login(process.env.BOT_TOKEN).catch(err => logger.error(err));
+client()
 
 const database = new Database(process.env.MONGO_URI);
 database.connect().then(() => {
