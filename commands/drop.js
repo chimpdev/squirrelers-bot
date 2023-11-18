0ac380440d1d4daa28b9de7671b1d3ae0723ee09
chimpdev <skyhancloud@gmail.com>
2023-11-18 06:31:39 +0300

    refactor(commands): Refactor the drop command to manage squirrel picture dropping

--- commands/drop.js
+++ commands/drop.js
@@ -3,26 +3,42 @@ import Discord from 'discord.js'
 export default {
   data: new Discord.SlashCommandBuilder()
     .setName('drop')
-    .setDescription('I\'ll drop a random squirrel picture every 1 hour.')
-    .addChannelOption(option => option.setName('channel').setDescription('The channel to drop squirrel pictures in. Leave blank to disable dropping squirrel pictures.'))
+    .setDescription('Manage the squirrel picture dropping.')
+    .addSubcommand(subcommand => subcommand.setName('set').setDescription('Set the channel to drop squirrel pictures in.')
+      .addChannelOption(option => option.setName('channel').setDescription('The channel to drop squirrel pictures in.')))
+    .addSubcommand(subcommand => subcommand.setName('stop').setDescription('Stop dropping squirrel pictures.'))
     .toJSON(),
   execute: async interaction => {
     await interaction.deferReply();
 
-    try {
-      const channel = interaction.options.getChannel('channel');
-      if (!channel) {
-        await database.delete(`drop-squirrel.${interaction.guild.id}`);
-        return interaction.followUp('I\'ll stop dropping squirrel pictures.');
-      } else {
-        if (![Discord.ChannelType.GuildAnnouncement, Discord.ChannelType.GuildText].includes(channel.type)) return interaction.followUp('I can only drop squirrel pictures in text channels.');
+    const subcommand = interaction.options.getSubcommand();
+    switch (subcommand) {
+      case 'set':
+        try {
+          const channel = interaction.options.getChannel('channel');
+          if (!channel) {
+            await database.delete(`drop-squirrel.${interaction.guild.id}`);
+            interaction.followUp('I\'ll stop dropping squirrel pictures.');
+            break;
+          } else {
+            if (![Discord.ChannelType.GuildAnnouncement, Discord.ChannelType.GuildText].includes(channel.type)) return interaction.followUp('I can only drop squirrel pictures in text channels.');
 
-        await database.set(`drop-squirrel.${interaction.guild.id}`, channel.id);
-        return interaction.followUp(`I'll drop squirrel pictures in ${channel} every 1 hour.`);
-      }
-    } catch (error) {
-      logger.error(error);
-      return interaction.followUp('There was an error trying to set the channel D:');
-    }
+            await database.set(`drop-squirrel.${interaction.guild.id}`, channel.id);
+            interaction.followUp(`I'll drop squirrel pictures in ${channel} every 1 hour.`);
+            break;
+          }
+        } catch (error) {
+          logger.error(error);
+          interaction.followUp('There was an error trying to set the channel D:');
+          break;
+        };
+      case 'stop':
+        const channel = await database.get(`drop-squirrel.${interaction.guild.id}`);
+        if (!channel) return interaction.followUp('I\'m not dropping squirrel pictures in this server.');
+
+        await database.delete(`drop-squirrel.${interaction.guild.id}`);
+        interaction.followUp('I\'ll stop dropping squirrel pictures.');
+        break;
+    };
   }
 }
